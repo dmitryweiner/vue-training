60050e15d6c2e2ae0942720fcd0db652f1f5534d
dmitryweiner <dmitry.weiner@gmail.com>
2020-03-16 20:25:54 +0700

    Created component MessagesList

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <ul>
-      <li v-for="(item, index) in messages" :key="index">
-        {{ item.message }}
-      </li>
-    </ul>
+    <MessagesList :messages="messages"/>
     <form v-on:submit.prevent="sendMessage">
       <input type="text" v-model="nick"/><br>
       <textarea v-model="message"></textarea>
@@ -15,12 +11,14 @@
 
 <script>
 import axios from "axios";
+import MessagesList from "./components/MessagesList";
 
 const apiUrl = "http://localhost:3000";
 
 export default {
   name: 'App',
   components: {
+    MessagesList
   },
   data: () => ({
     messages: [],
---
+++ src/components/MessagesList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item, index) in messages" :key="index">
+                {{ item.message }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "MessagesList",
+        props: [
+            "messages"
+        ]
+    }
+
+</script>
+
+<style scoped>
+
+</style>
