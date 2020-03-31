333bf51aa7e001d7280329e547cee206bd679712
dmitryweiner <dmitry.weiner@gmail.com>
2020-03-31 15:21:22 +0700

    Added new component Message

---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+    <li >
+        <strong>{{ message.nick }}:</strong> {{ message.message }}
+    </li>
+</template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from 'vue-property-decorator';
+    import {IMessage } from '@/interfaces/messages.ts';
+
+    @Component
+    export default class Message extends Vue {
+        @Prop() message: IMessage;
+    }
+
+</script>
